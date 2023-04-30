ee2d38ced4cebfbacbc98ec8ccefd7ec1969356d
nNabakhteveli <nika.nabakhteveli1@gmail.com>
2023-04-30 22:34:03 +0400

    Add random approve/decline of transactions

--- EBank/EBank.cs
+++ EBank/EBank.cs
@@ -5,54 +5,9 @@ namespace EBank;
 
 public class EBank : IEBank
 {
-    private readonly IWalletRepository _walletRepository;
-    private readonly ITransactionsRepository _transactionsRepository;
-
-    public EBank(IWalletRepository walletRepository, ITransactionsRepository transactionsRepository)
+    public int ValidateTransfer()
     {
-        _walletRepository = walletRepository;
-        _transactionsRepository = transactionsRepository;
-    }
-
-    public async Task<int> ProcessDeposit(Deposit transaction, Deposit createdtransaction)
-    {
-        int status = 1;
-        var userWallet = await _walletRepository.GetWalletByUserIdAsync(transaction.UserId);
-        var recipientWallet = await _walletRepository.GetWalletByUserIdAsync(transaction.RecipientId);
-
-        if (userWallet.CurrentBalance < transaction.Amount || recipientWallet == null)
-        {
-            status = 2;
-            return status;
-        }
-
-        userWallet.CurrentBalance -= transaction.Amount;
-        recipientWallet.CurrentBalance += transaction.Amount;
-
-        await _walletRepository.UpdateWalletAsync(userWallet);
-        await _walletRepository.UpdateWalletAsync(recipientWallet);
-
-        return status;
-    }
-
-    public async Task<int> ProcessWithdraw(TransactionEntity transaction, TransactionEntity createdTransaction)
-    {
-        var status = 1;
-        var userWallet = await _walletRepository.GetWalletByUserIdAsync(transaction.UserId);
-
-        if (userWallet.CurrentBalance < transaction.Amount)
-        {
-            status = 2;
-            createdTransaction.Status = status;
-
-            userWallet.CurrentBalance += transaction.Amount;
-
-            await _transactionsRepository.UpdateAsync(createdTransaction);
-            await _walletRepository.UpdateWalletAsync(userWallet);
-
-            return status;
-        }
-
-        return status;
+        var random = new Random();
+        return random.Next(1, 3);
     }
 }
--- EBank/IEBank.cs
+++ EBank/IEBank.cs
@@ -1,9 +1,6 @@
-using EWallet.Domain.Models;
-
 namespace EBank;
 
 public interface IEBank
 {
-    Task<int> ProcessDeposit(Deposit transaction, Deposit createdtransaction);
-    Task<int> ProcessWithdraw(TransactionEntity transaction, TransactionEntity createdtransaction);
+    int ValidateTransfer();
 }
