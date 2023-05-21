609b660e2a7c0a0ef0b2aa290b01da8d41a2f6ff
nNabakhteveli <nika.nabakhteveli1@gmail.com>
2023-05-21 22:36:07 +0400

    Add payment processor project

---
+++ PaymentProcessor/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿const urlParams = new URLSearchParams(window.location.search);
+const amount = urlParams.get('amount');
+const transactionId = urlParams.get('transactionId');
+
+
+$(document).ready(() => {
+    $('.transaction-id-text').text(`Transaction ID: ${transactionId}`);
+    $('.main-text').text(`Accept or reject your payment of $${amount} here.`);
+
+    $('.accept-btn').on('click', () => {
+        $.ajax({
+            type: 'POST',
+            url: 'https://localhost:7039/Transactions/Api/AcceptDeposit',
+            data: { success: true, transactionId: transactionId},
+            success: data => {
+                window.location.href = `https://localhost:7039/Identity/Account/Wallet?success=true&amount=${amount}`;
+            }
+        });
+    });
+    
+    
+    $('.reject-btn').on('click', () => {
+        $.ajax({
+            type: 'POST',
+            url: 'https://localhost:7039/Transactions/Api/RejectDeposit',
+            data: { success: false, transactionId: transactionId},
+            success: data => {
+                window.location.href = "https://localhost:7039/Identity/Account/Wallet?success=false";
+            }
+        });
+    });
+});
+
+console.log(amount, transactionId);
