cc150bd521afb50de66f8c6d9b890efa9dd864da
nNabakhteveli <nika.nabakhteveli1@gmail.com>
2023-05-22 15:46:27 +0400

    Separate Deposit and Withdraw into separate controllers

--- EWallet/Controllers/WithdrawController.cs
+++ EWallet/Controllers/WithdrawController.cs
@@ -5,62 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EWallet.Controllers;
 
-public class MoneyTransferController : Controller
+public class WithdrawController : Controller
 {
     private readonly ITransactionsRepository _transactionsRepository;
     private readonly IEBank _bank;
 
-    public MoneyTransferController(ITransactionsRepository transactionsRepository,
+    public WithdrawController(ITransactionsRepository transactionsRepository,
         IEBank bank)
     {
         _transactionsRepository = transactionsRepository;
         _bank = bank;
-    }
-
-    [HttpPost]
-    [Route("/Transactions/Api/Deposit")]
-    public async Task<JsonResult> Deposit(TransactionEntity transaction)
-    {
-        transaction.CreateDate = DateTime.Now;
-
-        if (!ModelState.IsValid || transaction.Amount < 0)
-        {
-            transaction.Status = 2;
-
-            await _transactionsRepository.CreateAsync(transaction);
-            return Json(new { Success = false });
-        }
-
-        var createdTransaction = await _transactionsRepository.CreateAsync(transaction);
-
-
-        return Json(new { Success = true, Amount = createdTransaction.Amount, TransactionId = createdTransaction.Id });
-    }
-
-    [HttpPost]
-    [Route("/Transactions/Api/AcceptDeposit")]
-    public async Task<JsonResult> AcceptedDeposit(AppTransaction transaction)
-    {
-        // imitation of a bank
-        var transactionStatus = _bank.ValidateTransfer();
-
-        await _transactionsRepository.AcceptDeposit(transaction.TransactionId, transactionStatus);
-
-        if (transactionStatus == 1) return Json(new { success = true });
-
-        return Json(new { success = false });
-    }
-
-    [HttpPost]
-    [Route("/Transactions/Api/RejectDeposit")]
-    public async Task<JsonResult> RejectDeposit(AppTransaction transaction)
-    {
-        var oldTransaction = await _transactionsRepository.GetTransactionByIdAsync(transaction.TransactionId);
-
-        oldTransaction.Status = 2;
-        await _transactionsRepository.UpdateAsync(oldTransaction);
-
-        return Json(new { success = true });
     }
 
     [HttpPost]
